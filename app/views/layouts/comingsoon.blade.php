b6c1fa5a4c4675a4ef8e4315f26460aa016e6364
James Asher <onijim@gmail.com>
2014-07-30 03:53:23 +0000

    initial commit

---
+++ app/views/layouts/comingsoon.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>B is for Bryan</title>
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
+
+    <link href="css/comingsoon.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <h1>B is for Bryan</h1>
+        </div>
+    </div>
+
+    <div class="jumbotron cookie-mystery">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-med-12"><img class="img-responsive" src="../img/cookie-mystery.jpg" /></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container-fluid">
+            <h1>Coming Soon</h1>
+        </div>
+    </div>
+
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
+
+</body>
+</html>
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>B is for Bryan</title>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <div class="navbar navbar-default navbar-static-top">
+        <div class="container centered">
+            <h1 class="big-text">B is for Bryan</h1>
+        </div>
+    </div>
+
+    <div class="jumbotron cookie-mystery">
+        <img class="img-responsive" src="../img/cookie-mystery.jpg" />
+    </div>
+
+    <div class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container centered">
+            <h1 class="big-text">Coming Soon</h1>
+        </div>
+    </div>
+
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>
