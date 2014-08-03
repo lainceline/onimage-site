10366c327656297af49a05b4d774ad04297a26cd
James Asher <onijim@gmail.com>
2014-08-02 22:24:46 -0400

    add some security to the image uploader

--- app/controllers/ImageController.php
+++ app/controllers/ImageController.php
@@ -4,6 +4,9 @@ use Intervention\Image\ImageManagerStatic as ImageKit;
 
 class ImageController extends \BaseController {
 
+    protected $whitelist = array('jpg', 'jpeg', 'png', 'gif');
+    protected $mimeWhitelist = array('image/gif', 'image/jpeg', 'image/png');
+
     public function all()
     {
         return Image::all();
@@ -18,11 +21,29 @@ class ImageController extends \BaseController {
     public function upload()
     {
         $file = Input::file('file');
-        
+
+        $input = array('image' => $file);
+        $rules = array('image' => 'image');
+
+        $validator = Validator::make($input, $rules);
+
         $destinationPath = 'public/uploads/';
 
-        $realFilename = $file->getClientOriginalName();
+        $origFilename = $file->getClientOriginalName();
         $extension = $file->getClientOriginalExtension();
+        $mimetype = $file->getMimeType();
+
+        if (!in_array($extension, $this->whitelist)) {
+            return Response::json('Supported extensions: jpg, jpeg, gif, png', 400);
+        }
+
+        if (!in_array($mimetype, $this->mimeWhitelist)) {
+            return Response::json('Error: this is not an image', 400);
+        }
+
+        if ($validator->fails()) {
+            return Response::json('Error: Not an image');
+        }
 
         $filename = str_random(12).'.'.$extension;
 
@@ -31,7 +52,7 @@ class ImageController extends \BaseController {
         if ($image) {
             $dbimage = new Image();
             $dbimage->filename = 'uploads/'.$filename;
-            $dbimage->title = $realFilename;
+            $dbimage->title = $origFilename;
             $dbimage->save();
             return $dbimage;
         } else {
