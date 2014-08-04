6c8c02c3b712a4d451dd753dfe0cedd4df8fd18f
James Asher <onijim@gmail.com>
2014-08-03 21:25:46 -0400

    fix for the single file upload buttons.  animation fixes.

--- public/js/directives/imageDirectives.js
+++ public/js/directives/imageDirectives.js
@@ -48,11 +48,11 @@ angular.module('imageDirectives', [])
                 });
 
                 myDropzone.on('addedfile', function(file) {
-                    file.previewElement.querySelector('.start').onclick = function() {myDropzone.processQueue(file); };
+                    file.previewElement.querySelector('.start-single').onclick = function() {myDropzone.processFile(file); };
                 });
 
                 // Update the total progress bar
-                myDropzone.on("totaluploadprogress", function(progress) {
+                myDropzone.on("uploadprogress", function(file, progress) {
                     elem.find("#total-progress .progress-bar").get(0).style.width = progress + "%";
                 });
 
@@ -60,7 +60,7 @@ angular.module('imageDirectives', [])
                     // Show the total progress bar when upload starts
                     elem.find("#total-progress").get(0).style.opacity = "1";
                     // And disable the start button
-                    file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+                    file.previewElement.querySelector(".start-single").setAttribute("disabled", "disabled");
                 });
 
                 // Hide the total progress bar when nothing's uploading anymore
@@ -70,10 +70,17 @@ angular.module('imageDirectives', [])
 
                 myDropzone.on('success', function(file, image) {
                     scope.images.push(image);
+
+                    //trigger animations to hide the file row and the total progress bar
                     timer(function() {
                         elem.find('.file-row').fadeOut('slow');
                     }, 5000);
-                })
+
+                    //only reset the width once the bar is completely hidden to avoid jarring visuals
+                    elem.find("#total-progress").fadeOut(400, function() {
+                        elem.find('#total-progress .progress-bar').get(0).style.width = '0%';
+                    });
+                });
 
                 // Setup the buttons for all transfers
                 // The "add files" button doesn't need to be setup because the config
