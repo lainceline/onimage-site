83c4444835f0edd7580b2f9afbe50dd9abc9dd96
James Asher <onijim@gmail.com>
2014-08-04 19:50:31 -0400

    add oauth service providers, aliases, config files

---
+++ app/config/packages/cerbero/oauth/yahoo.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	/**
+	 * Consumer key provided by Yahoo!
+	 */
+	'consumer_key' => '',
+
+	/**
+	 * Consumer secret key provided by Yahoo!
+	 */
+	'consumer_secret' => '',
+
+	/**
+	 * Application ID provided by Yahoo!
+	 */
+	'application_id' => ''
+
+);
