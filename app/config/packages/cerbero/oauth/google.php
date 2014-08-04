ad652d537561daf016b173dd20c5848ab64d0b5e
James Asher <onijim@gmail.com>
2014-08-04 19:56:00 -0400

    setting up google oauth

--- app/config/packages/cerbero/oauth/google.php
+++ app/config/packages/cerbero/oauth/google.php
@@ -5,11 +5,11 @@ return array(
 	/**
 	 * Client ID provided by Google
 	 */
-	'client_id' => '',
+	'client_id' => $_ENV['GOOGLE_CLIENT_ID'],
 
 	/**
 	 * Client secret key provided by Google
 	 */
-	'client_secret' => ''
+	'client_secret' => $_ENV['GOOGLE_CLIENT_SECRET']
 
 );
