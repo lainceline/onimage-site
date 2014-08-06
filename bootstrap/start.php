10e48ce56ee3f3d4a9cb3730f1d6c4964a27cb36
James Asher <onijim@gmail.com>
2014-08-05 20:29:26 -0400

    setup a decent environments in bootstrap start

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('james-mba-arch'),
+	'local' => array('localhost'),
+	'production' => array('*.com')
 
 ));
 
